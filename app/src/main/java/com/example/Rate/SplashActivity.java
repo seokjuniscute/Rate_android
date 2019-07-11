8874aaf998ec44c80ac5480e64febe583588d562
greenjjun <greenjjun@naver.com>
2019-07-11 19:04:46 +0900

    Add login

--- app/src/main/java/com/example/Rate/SplashActivity.java
+++ app/src/main/java/com/example/Rate/SplashActivity.java
@@ -18,10 +18,10 @@ public class SplashActivity extends AppCompatActivity {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(getBaseContext(), MainActivity.class);
+                Intent intent = new Intent(getBaseContext(), LoginActivity.class);
                 startActivity(intent);
                 finish();
             }
-        }, 2000);
+        }, 1000);
     }
 }
